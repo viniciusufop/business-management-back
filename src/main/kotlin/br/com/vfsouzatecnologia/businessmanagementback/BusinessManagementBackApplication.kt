ac58179eec6426498043006be88f981fc9bc5b5b
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-04-24 12:45:47 -0300

    inicio do projeto

---
+++ src/main/kotlin/br/com/vfsouzatecnologia/businessmanagementback/BusinessManagementBackApplication.kt
@@ -0,0 +1,11 @@
+package br.com.vfsouzatecnologia.businessmanagementback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BusinessManagementBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<BusinessManagementBackApplication>(*args)
+}
